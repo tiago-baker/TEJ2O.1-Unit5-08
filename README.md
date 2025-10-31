3dd7d99112e1e29a5510d2c1cecb31fc9ff5ff2e
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-10-31 00:27:04 -0400

    done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TEJ2O ⇢ Unit #X-YY
+# TEJ2O ⇢ Unit #5-08
 
 [![CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-blue.svg)](./LICENSE)
 
--- main.ts
+++ main.ts
@@ -1,8 +1,43 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Tiago Baker
+ * Created on: Oct 2025
+ * This program tepper motor back and forth
 */
 
-basic.showString('Hello, World!')
+// Variables
+let distanceToObject: number = 0
+
+while (true) {
+    // Clear screen and read sonar distance
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+
+    // If object is closer than 10 cm, reverse and turn right
+    if (distanceToObject < 10) {
+
+
+        // Turn 90 degrees
+        let degrees=90
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
+
+        // Reverse both motors for about 10 cm
+        robotbit.StpCarMove(-10, 48)
+        basic.pause(500)
+    }
+
+    // If distance is 10 cm or more, move forward 10 cm
+    if (distanceToObject >= 10) {
+        // move forward 10cm
+        basic.showIcon(IconNames.Yes)
+        robotbit.StpCarMove(10, 48)
+        basic.pause(500)
+
+    }
+
+}
